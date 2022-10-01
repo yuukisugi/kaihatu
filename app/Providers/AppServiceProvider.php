3d2e1c012dbf70f644d05da6c813146abc043a44
EC2 Default User <ec2-user@ip-172-31-31-128.ap-northeast-1.compute.internal>
2022-10-01 13:19:52 +0000

    10月1日

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //if (\App::environment('production')) {
-        \URL::forceScheme('https');
+        //
+        if (\App::environment('production')) 
+            \URL::forceScheme('https');
     }
 }
